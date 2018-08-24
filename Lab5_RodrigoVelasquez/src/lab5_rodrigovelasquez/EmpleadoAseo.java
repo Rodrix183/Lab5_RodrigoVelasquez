747b5e12303e01ee1ced437c48532196e4b349b1
rodrix183 <rodrix183@hotmail.com>
2018-08-24 13:52:08 -0600

    atributos clase EmpleadoAseo

--- Lab5_RodrigoVelasquez/src/lab5_rodrigovelasquez/EmpleadoAseo.java
+++ Lab5_RodrigoVelasquez/src/lab5_rodrigovelasquez/EmpleadoAseo.java
@@ -1,5 +1,32 @@
 package lab5_rodrigovelasquez;
 
-public class EmpleadoAseo {
+import java.util.ArrayList;
+import java.util.Date;
+
+public class EmpleadoAseo extends Empleado {
+
+    private ArrayList<Funcion> lista_funciones = new ArrayList<>();
+
+    public EmpleadoAseo() {
+        super();
+    }
+
+    public EmpleadoAseo(String nombre, Date fecha, String user, String password, String email, String puesto) {
+        super(nombre, fecha, user, password, email, puesto);
+    }
+
+    public ArrayList<Funcion> getLista_funciones() {
+        return lista_funciones;
+    }
+
+    public void setLista_funciones(ArrayList<Funcion> lista_funciones) {
+        this.lista_funciones = lista_funciones;
+    }
+
+    @Override
+    public String toString() {
+        return "Lista Funciones: " + lista_funciones;
+    }
+
     
 }

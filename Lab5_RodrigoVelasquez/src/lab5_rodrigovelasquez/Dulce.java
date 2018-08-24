99156f32df30ff8bf19d3960a749e8849cf8f376
rodrix183 <rodrix183@hotmail.com>
2018-08-24 13:39:44 -0600

    Clase Dulce

---
+++ Lab5_RodrigoVelasquez/src/lab5_rodrigovelasquez/Dulce.java
@@ -0,0 +1,41 @@
+package lab5_rodrigovelasquez;
+
+class Dulce {
+    private String nombre;
+    private String sabor;
+    private String categoria;
+
+    public Dulce() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+    
+    
+}

3f9f6abe3f1a39bf102e5b391b409d2d9a842186
rodrix183 <rodrix183@hotmail.com>
2018-08-24 13:45:48 -0600

    Empleado boleteria

--- Lab5_RodrigoVelasquez/src/lab5_rodrigovelasquez/EmpleadoBoleteria.java
+++ Lab5_RodrigoVelasquez/src/lab5_rodrigovelasquez/EmpleadoBoleteria.java
@@ -1,14 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab5_rodrigovelasquez;
 
-/**
- *
- * @author rodri
- */
-public class EmpleadoBoleteria {
+import java.util.ArrayList;
+import java.util.Date;
+
+public class EmpleadoBoleteria extends Empleado {
+
+    private ArrayList <Pelicula> lista_peliculas = new ArrayList<>();
+    
+    public EmpleadoBoleteria() {
+        super();
+    }
+
+    public EmpleadoBoleteria(String nombre, Date fecha, String user, String password, String email, String puesto) {
+        super(nombre, fecha, user, password, email, puesto);
+    }
+
+    public ArrayList<Pelicula> getLista_peliculas() {
+        return lista_peliculas;
+    }
+
+    public void setLista_peliculas(ArrayList<Pelicula> lista_peliculas) {
+        this.lista_peliculas = lista_peliculas;
+    }
+
+    @Override
+    public String toString() {
+        return "EmpleadoBoleteria{" + "lista_peliculas=" + lista_peliculas + '}';
+    }
+
+    
+    
+    
     
 }
